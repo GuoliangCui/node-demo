411c2e4b20e7277c1ae4b26163a690cba9a6401f
cuiguoliang <63354361@qq.com>
2016-10-13 15:38:35 +0800

    提交测试代码

---
+++ server.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Administrator on 2016/10/13.
+ */
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+    res.send('Hello World')
+})
+app.get('/test', function (req, res) {
+    res.send('test page')
+})
+app.listen(3000);
